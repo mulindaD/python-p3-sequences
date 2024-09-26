6a714f7758555dfdfb52c80eebfccd38b711501e
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-09-26 15:49:31 +0300

    Set up Fibonacci with loops

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+    print(sequence[ :length])
+    return sequence[ :length]
